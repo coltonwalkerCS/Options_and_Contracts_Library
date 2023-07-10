d1f42e17a8f7b7523bd90eca83cf65935c8e5bf1
colewalker <cmw9nb@mst.edu>
2023-07-10 17:24:53 -0500

    Bond futures additions

--- Commodities_Future_Pricing_Lib.py
+++ Commodities_Future_Pricing_Lib.py
@@ -1,17 +1,21 @@
 import math
+
+
 # --- Physical commodities (Grains, Energy Products, Precious Metals, etc) ---
+
 
 # Find the forward price for a physical commodity
 def physicalCommodityForwardPrice(commodity_price, time_to_maturity, interest_rate, annual_storage_rate,
                                   annual_insurance_cost):
-    forward_price = commodity_price * (1 + interest_rate * time_to_maturity) + (annual_storage_rate * time_to_maturity)\
+    forward_price = commodity_price * (1 + interest_rate * time_to_maturity) + (annual_storage_rate * time_to_maturity) \
                     + (annual_insurance_cost * time_to_maturity)
     return forward_price
 
 
-def getConvenienceYield_XMoContract(numMonths, forward_price, interest_rate, annual_storage_cost, annual_insurance_cost, cash_price):
+def getConvenienceYield_XMoContract(numMonths, forward_price, interest_rate, annual_storage_cost, annual_insurance_cost,
+                                    cash_price):
     commodity_price = forward_price - (annual_storage_cost + annual_insurance_cost) * (numMonths / 12)
-    commodity_price /= (1 + (interest_rate / 100) * (numMonths/12))
+    commodity_price /= (1 + (interest_rate / 100) * (numMonths / 12))
     return cash_price - commodity_price
 
 
@@ -36,13 +40,12 @@ def generateDivPayments(time_to_maturity, dividend_payment, time_to_next_div):
 # Return the forward price
 def stockForwardPrice(stock_price, time_to_maturity, interest_rate, each_dividend_payment,
                       time_remaining_after_each_div, applicable_interest_rate):
-
-    forwardPrice = stock_price + (stock_price * (interest_rate/100) * (time_to_maturity/12))
+    forwardPrice = stock_price + (stock_price * (interest_rate / 100) * (time_to_maturity / 12))
 
     dividend_sum = 0
     for i in range(len(each_dividend_payment)):
         dividend_sum += each_dividend_payment[i] * (1 + (applicable_interest_rate[i] / 100) *
-                                                    (time_remaining_after_each_div[i]/12))
+                                                    (time_remaining_after_each_div[i] / 12))
 
     forwardPrice -= dividend_sum
 
@@ -51,7 +54,30 @@ def stockForwardPrice(stock_price, time_to_maturity, interest_rate, each_dividen
 
 def simpleStockForwardPrice(stock_price, interest_rate, time_to_maturity, dividend, time_to_next_div):
     numDividends = math.floor((time_to_maturity - time_to_next_div) / 6) + 1
-    forwardPrice = stock_price * (1 + interest_rate * time_to_maturity) - (numDividends * dividend)
-    print(forwardPrice)
-    assert(forwardPrice == 69.02)
+    forwardPrice = stock_price * (1 + (interest_rate / 100) * time_to_maturity / 12) - (numDividends * dividend)
     return forwardPrice
+
+
+# ---- BONDS ----
+
+def bondForwardPrice(bond_price, time_to_maturity, rate_interest, each_coupon_prior_to_exp, time_rem_after_coupon_list,
+                     applicable_interest_rate_from_each_coupon):
+    sum_interest_coupons = 0
+
+    for i in range(0, len(each_coupon_prior_to_exp)):
+        sum_interest_coupons += (
+                    each_coupon_prior_to_exp[i] * (1 + (applicable_interest_rate_from_each_coupon[i] / 100) *
+                                                   (time_rem_after_coupon_list[i] / 12)))
+
+    forwardPrice = (bond_price * (1 + (rate_interest / 100) * (time_to_maturity / 12)) - sum_interest_coupons)
+    return forwardPrice
+
+
+def generateCouponMonthPayments(time_to_maturity, next_coupon_payment):
+    coupon_ind_time = time_to_maturity - next_coupon_payment
+    coupon_times = [coupon_ind_time]
+    coupon_ind_time -= 6
+    while coupon_ind_time > 0:
+        coupon_times.append(coupon_ind_time)
+        coupon_ind_time -= 6
+    return coupon_times
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Commodities_Future_Pricing_Lib import getConvenienceYield_XMoContract, stockForwardPrice, generateDivPayments, \
-    simpleStockForwardPrice
+    simpleStockForwardPrice, generateCouponMonthPayments, bondForwardPrice
+
+# To test the library and assert the correct values
 
 # Commodity Example
 F = 77.40
@@ -10,7 +12,8 @@ annual_insurance_cost = 0.6
 conv_yield = getConvenienceYield_XMoContract(3, F, interest_rate, annual_storage_cost, annual_insurance_cost, 76.25)
 
 assert(conv_yield == 1.25)
-print(f"Convenience Yield: {conv_yield}")
+print(f"Convenience Yield: {conv_yield} \n")
+
 
 # Stock example
 stock_Price = 67
@@ -25,7 +28,32 @@ divTimes, divPayments = generateDivPayments(time_to_maturity, semiannual_div_pay
 forwardPriceStock = stockForwardPrice(stock_Price, time_to_maturity, interest_rate_stock, divPayments, divTimes,
                                       applicable_interest_rate)
 assert(forwardPriceStock >= 69.006)
-print(f"Forward stock price: {forwardPriceStock}")
+print(f"Forward stock price: {forwardPriceStock} \n")
 
 simpleForwardPriceStock = simpleStockForwardPrice(stock_Price, interest_rate_stock, time_to_maturity, semiannual_div_payment, time_to_next_div_payment)
-print(f"Simple forward stock price: {simpleForwardPriceStock}")
+print(f"Simple forward stock price: {simpleForwardPriceStock} \n")
+
+
+# Bond Example
+
+bond_Price = 109.76
+# 10 months
+time_to_maturity_bond = 10
+# 8%
+interest_rate_bond = 8
+# 5.25%
+semiannual_coupon_payment = 5.25
+# 2 months
+time_to_next_coupon_payment = 2
+# Assumed coupon rates
+coupon_bond_rates = [8.2, 8.5]
+# Semi-annual coupon payments
+coupons_exp_prior = [5.25, 5.25]
+coupon_times = generateCouponMonthPayments(time_to_maturity_bond, time_to_next_coupon_payment)
+
+# time_rem_after_coupon_list,
+simpleBondForwardBondPrice = bondForwardPrice(bond_Price, time_to_maturity_bond, interest_rate_bond, coupons_exp_prior,
+                                              coupon_times, coupon_bond_rates)
+
+assert(106.2159 <= simpleBondForwardBondPrice <= 106.216)
+print(f"Forward bond price: {simpleBondForwardBondPrice} \n")
