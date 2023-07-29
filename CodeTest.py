32f610877917a8da25c879d81c91b3d7acd89729
colewalker <cmw9nb@mst.edu>
2023-07-29 18:28:51 -0500

    CommoditiesFuturePricing name change

--- CodeTest.py
+++ CodeTest.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-from Commodities_Future_Pricing_Lib import getConvenienceYield_XMoContract, stockForwardPrice, generateDivPayments, \
+from CommoditiesFuturePricing import getConvenienceYield_XMoContract, stockForwardPrice, generateDivPayments, \
     simpleStockForwardPrice, generateCouponMonthPayments, bondForwardPrice
 from PricingModels import call_option_expected_value, get_theoretical_value_of_contract, put_option_expected_value, \
     get_scaled_volatility, black_scholes_model_option_price, calc_theta_for_atm_option
